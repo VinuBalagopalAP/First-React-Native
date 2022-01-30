2ef2aa5083a12fb75ffd8bff774382ac9324356c
VinuBalagopalAP <vinubalagopalap@gmail.com>
2022-01-30 13:53:28 +0530

    Using state Hook

--- App.js
+++ App.js
@@ -1,24 +1,29 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, {useState} from 'react';
 
-import React from 'react';
-
-import {Button, Linking, StyleSheet, Text, View} from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
 
 const App = () => {
+  const [name, setName] = useState('Vinu');
+  const [session, setSession] = useState({number: 6, title: 'state'});
+
+  const [current, setCurrent] = useState(true);
+
+  const onClickHandler = () => {
+    setName('Vinu Balagopal A P');
+    setSession({number: 7, title: 'Style'});
+    setCurrent(false);
+  };
+
   return (
     <View style={styles.body}>
-      <Text style={styles.text}>Vinu Balagopal A P</Text>
-      <Button
-        title="YouTube Channel"
-        onPress={() => {
-          Linking.openURL('https://www.youtube.com/c/vinubalagopalap');
-        }}></Button>
+      <Text style={styles.text}>{name} </Text>
+      <Text style={styles.text}>
+        This is session {session.number} and about {session.title}
+      </Text>
+      <Text style={styles.text}>
+        {current ? 'current session' : 'next session'}
+      </Text>
+      <Button title="Update State" onPress={onClickHandler} />
     </View>
   );
 };
